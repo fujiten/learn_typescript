a24d36b79bd39de4f64aab4d10adb37d530cbaf7
fujiten <hikaru.fujita.0903@gmail.com>
2020-01-19 18:31:50 +0900

    module_es6

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,6 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="utl/validation.ts" />
-/// <reference path="component/project-input.ts" />
-/// <reference path="component/project-list.ts" />
-/// <reference path="component/project-item.ts" />
+import { ProjectInput } from './components/project-input.js';
+import { ProjectList } from './components/project-list.js';
 
-namespace App {
-
-  new ProjectInput()
-  new ProjectList('active')
-  new ProjectList('finished')
-
-}
-
-
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
